3226468679d65893eccf2f07db7a7b1df1d5afd3
SudarshanShah <sudarshanshah911@gmail.com>
2023-06-27 02:47:01 +0530

    constants in Kotlin

---
+++ src/main/kotlin/Constant.kt
@@ -0,0 +1,9 @@
+// static final PI = 3.14 --> equivalent of Java
+// The value is known before code execution
+// It can only be defined outside main()
+const val PI = 3.14
+
+fun main() {
+    val name = "Sudarshan" // read-only
+    var i = 0 // mutable
+}
