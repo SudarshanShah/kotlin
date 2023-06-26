c4f3d2553e729494fbb003cbf5be960b5cfe57e0
SudarshanShah <sudarshanshah911@gmail.com>
2023-06-27 02:36:33 +0530

    Type Inference in Kotlin

---
+++ src/main/kotlin/TypeInference.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // No need to specifically write type when declaring variables and assigning values
+    // Kotlin infers the type automatically for every variable
+    var i = 21
+    var l = 100L
+    var d = 3.14
+    var f = 3.14F
+    var b = true
+    var s = "Jetbrains"
+    var c = 'K'
+}
