13ecd1eecd3e823c711b6f32e85f8c0fb61eb67e
SudarshanShah <sudarshanshah911@gmail.com>
2023-06-27 02:31:22 +0530

    Variables and Data Types

---
+++ src/main/kotlin/DataTypes.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var i: Int = 21
+    var l: Long = 100L
+    var d: Double = 3.14
+    var f: Float = 3.14F
+    var b: Boolean = true
+    var s: String = "Jetbrains"
+    var c: Char = 'K'
+
+    // Any is superclass for all Data type classes
+    // NOT RECOMMENDED TO USE 'Any' data type
+    var a: Any = 20
+}
---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var name: String = "Sudarshan"
+    var age: Int = 26
+    println(name)
+    println(age)
+    println("Name: ${name}  Age: ${age}")
+}
