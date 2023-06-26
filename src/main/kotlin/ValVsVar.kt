043a5a00c063dd4cd7bbe6686a572e7a6670da93
SudarshanShah <sudarshanshah911@gmail.com>
2023-06-27 02:40:13 +0530

    Val vs Var keyword

---
+++ src/main/kotlin/ValVsVar.kt
@@ -0,0 +1,7 @@
+fun main() {
+    // Immutable variable -> read-only
+    // final String brand = "Sudarshan"  --> equivalent in Java
+    val brandVal = "Sudarshan"
+    var brandVar = "Sudarshan"
+    brandVar = "Sudarshan Shah"
+}
