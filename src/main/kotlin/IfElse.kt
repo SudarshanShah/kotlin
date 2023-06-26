61d86a8ed8f3de5de679a6bcd174725540c01ac1
SudarshanShah <sudarshanshah911@gmail.com>
2023-06-27 03:42:05 +0530

    If-Else expression

---
+++ src/main/kotlin/IfElse.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val n1 = 30
+    val n2 = 20
+
+    if (n1 > n2) {
+        println(":)")
+    } else if (n1 == n2) {
+        println("(::)")
+    } else {
+        println(":(")
+    }
+
+    val res = if (n1 > n2) {
+        ":)"
+    } else {
+        ":("
+    }
+    println("If-Else evaluates to $res")
+}
