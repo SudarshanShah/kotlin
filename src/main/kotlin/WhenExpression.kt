0bdc0729dd426ddc5b332ba4f80a887751b59152
SudarshanShah <sudarshanshah911@gmail.com>
2023-06-28 04:54:32 +0530

    when expression

---
+++ src/main/kotlin/WhenExpression.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val gender = "F"
+
+    val g = when(gender) {
+        "F" -> "Female"
+        "M" -> "Male"
+        else -> "Others"
+    }
+
+    println(g)
+
+    val age = 25
+
+    when(age) {
+        in 13..19 -> println("Teenager")
+        in 20..25 -> println("Youthful")
+        else -> println()
+    }
+}
