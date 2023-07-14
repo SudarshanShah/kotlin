8d991cf2624f4a7371dd3ffff0833179320fec60
SudarshanShah <sudarshanshah911@gmail.com>
2023-07-14 05:53:18 +0530

    All about basics of Arrays

---
+++ src/main/kotlin/Arrays.kt
@@ -0,0 +1,23 @@
+fun main() {
+    val names = arrayOf("Superman", "Batman", "Wonder Woman")
+    println(names.contentToString())
+    println(names[0])
+    names[1] = "Dark Knight"
+    println(names[1])
+    println(names.size)
+
+    if ("Superman" in names) {
+        println("Found")
+    } else println("Not found")
+
+    // array pre-filled with null values
+    val arrayOfNulls = arrayOfNulls<String>(5)
+    arrayOfNulls[3] = "Kotlin"
+    println(arrayOfNulls.contentToString())
+
+    // returns an empty array
+    val emptyArray = emptyArray<String>()
+    println(emptyArray.contentToString())
+
+
+}
