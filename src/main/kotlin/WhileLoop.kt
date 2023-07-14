cdf5e39d4a5e4b624f322b3f3bafacdaa0d2b387
SudarshanShah <sudarshanshah911@gmail.com>
2023-07-14 06:49:15 +0530

    While and Do-while loops in Kotlin

---
+++ src/main/kotlin/WhileLoop.kt
@@ -0,0 +1,30 @@
+fun main() {
+    var number = 1
+    while (number <= 5) {
+        println(number)
+        number++
+    }
+
+    while (number >= 0) {
+        println(number)
+        number--
+    }
+
+    val brand = "Sudarshan"
+    var index = 0
+    while (index < brand.length) {
+        println("Brand ${brand[index]}")
+        index++
+    }
+
+    val nums = arrayOf(1, 2, 3, 4, 5)
+    var idx = 0
+    while (idx < nums.size) {
+        println("Element is ${nums[idx]}")
+        idx++
+    }
+
+    do {
+        println("Hello")
+    } while (false)
+}
