4a3ea49b671d04e91267f4f47708eab2f783b809
SudarshanShah <sudarshanshah911@gmail.com>
2023-07-14 06:07:16 +0530

    Basics of List in Kotlin

---
+++ src/main/kotlin/Lists.kt
@@ -0,0 +1,34 @@
+fun main() {
+    // It creates immutable list
+    val names = listOf(
+        "Sudarshan",
+        "Tony",
+        "Steve",
+        "Clark",
+        "Bruce"
+    )
+
+    println(names)
+    println(names[1])
+    println(names.size)
+    println(names.contains("Bruce"))
+    println(names.first())
+    println(names.last())
+    println(names.indexOf("Clark"))
+    println(names.lastIndexOf("Steve"))
+
+    // It creates a mutable list
+    val heroes = mutableListOf(
+        "Tony",
+        "Steve",
+        "Clark",
+        "Bruce"
+    )
+
+    println(heroes)
+    heroes.add("Peter")
+    heroes.add("Diana")
+    heroes.add(1, "Natasha")
+    println(heroes)
+    println(heroes.isEmpty())
+}
