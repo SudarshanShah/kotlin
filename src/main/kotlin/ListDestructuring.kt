788376665cab8aac9336d3ded4ceb7bfe933ac95
SudarshanShah <sudarshanshah911@gmail.com>
2023-07-14 06:12:33 +0530

    Destructuring in Kotlin

---
+++ src/main/kotlin/ListDestructuring.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val names = listOf(
+        "Tony",
+        "Steve",
+        "Clark",
+        "Bruce"
+    )
+
+    // destructuring the list/array
+    val (t, s, c) = names
+    println("$t $s $c")
+}
