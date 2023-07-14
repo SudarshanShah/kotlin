02d2787257dabb1cedc1f3073907711c56112bbf
SudarshanShah <sudarshanshah911@gmail.com>
2023-07-14 06:33:19 +0530

    Loops with range and steps in Kotlin

---
+++ src/main/kotlin/LoopRanges.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    // printing numbers in range
+    for (i in 1..5) {
+        println(i)
+    }
+
+    // to print range in reverse
+    for (i in 10 downTo 1) println(i)
+
+    // using steps
+    for (i in 1..10 step 2) println(i)
+}
