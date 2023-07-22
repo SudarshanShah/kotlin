9099ad188939654373c33e5e2f1f939f46fe8871
SudarshanShah <sudarshanshah911@gmail.com>
2023-07-22 06:23:37 +0530

    Constructors in Kotlin

---
+++ src/main/kotlin/Constructors.kt
@@ -0,0 +1,22 @@
+fun main() {
+    Constructors(1)
+    // If a class have primary constructor with default value of all fields,
+    // compiler generates new parameterless constructor which uses the default values.
+    val  obj1 = Constructors1()
+    val obj2 = Constructors1("Constructor")
+}
+
+class Constructors {
+    init {
+        println("in the Init block")
+        println("code in Init block is part of primary constructor")
+    }
+
+    constructor(i: Int) {
+        println("constructor $i")
+    }
+}
+
+class Constructors1(val name: String = "Hello") {
+
+}
