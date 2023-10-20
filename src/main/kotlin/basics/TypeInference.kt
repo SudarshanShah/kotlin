eef11e01efe098e7ae733d6be21f157a6b9cc664
SudarshanShah <sudarshanshah911@gmail.com>
2023-10-21 01:53:18 +0530

    Small changes

--- src/main/kotlin/basics/TypeInference.kt
+++ src/main/kotlin/basics/TypeInference.kt
@@ -3,11 +3,11 @@ package basics
 fun main() {
     // No need to specifically write type when declaring variables and assigning values
     // Kotlin infers the type automatically for every variable
-    var i = 21
-    var l = 100L
-    var d = 3.14
-    var f = 3.14F
-    var b = true
-    var s = "Jetbrains"
-    var c = 'K'
+    var i = 21  // Int
+    var l = 100L // Long
+    var d = 3.14  // Double
+    var f = 3.14F // Float
+    var b = true // Boolean
+    var s = "Jetbrains" // String
+    var c = 'K' // Char
 }
--- src/main/kotlin/basics/Variables.kt
+++ src/main/kotlin/basics/Variables.kt
@@ -1,9 +1,9 @@
 package basics
 
 fun main() {
-    var name: String = "Sudarshan"
-    var age: Int = 26
+    val name: String = "Sudarshan"
+    val age: Int = 26
     println(name)
     println(age)
-    println("Name: ${name}  Age: ${age}")
+    println("Name: $name  Age: $age")
 }
